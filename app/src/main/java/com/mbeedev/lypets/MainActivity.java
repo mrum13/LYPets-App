12dc9cceee86fcd4e84d517473d6e44977abe6ef
Rum <muhammadrum135@gmail.com>
2021-05-29 17:26:25 +0800

    29 Mei 2021 WebView Version 1.0

---
+++ app/src/main/java/com/mbeedev/lypets/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mbeedev.lypets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView web = (WebView) findViewById(R.id.web_view);
+        web.loadUrl("https://lypets.id/");
+        web.setWebViewClient(new WebViewClient());
+
+    }
+}
